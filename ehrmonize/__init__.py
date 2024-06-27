170301cef16da3df4725fabf2c027b8b5838c1b7
joamats <joaocarlosm00@gmail.com>
2024-06-27 14:38:51 -0400

    modify docstrings

--- ehrmonize/__init__.py
+++ ehrmonize/__init__.py
@@ -3,4 +3,15 @@ from .ehrmonize import EHRmonize
 __author__ = "João Matos <joao.matos@duke.edu>, A. Ian Wong"
 __version__ = "0.1.0a4"
 
-__all__ = ['EHRmonize']
+__all__ = [
+    'EHRmonize',
+    'config_prompting',
+    'evaluate',
+    'predict',
+    'set_task',
+    'task_binary_drug',
+    'task_custom',
+    'task_generic_drug',
+    'task_generic_route',
+    'task_multiclass_drug',
+]
