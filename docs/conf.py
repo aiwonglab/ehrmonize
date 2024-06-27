3b01def4d6087ddb7d97453999e486cb48742fca
joamats <joaocarlosm00@gmail.com>
2024-06-27 11:53:21 -0400

    update html theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx-immaterial'
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
